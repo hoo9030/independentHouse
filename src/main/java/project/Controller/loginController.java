8d3f04683a5345c6aae50e752de6b8ef03cbb36d
hoo9030 <hoo9030@naver.com>
2016-10-25 13:13:46 +0900

    본격적인 기능구현 시작 - 로그인

---
+++ src/main/java/project/Controller/loginController.java
@@ -0,0 +1,15 @@
+package project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class loginController {
+	
+	@RequestMapping("login")
+	public String login(Model model){
+		return "index";
+	}
+	
+}
